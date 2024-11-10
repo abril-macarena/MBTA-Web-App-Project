e0af741e6d2e701894c894cd737a5bb828dad185
abril-macarena <138411863+abril-macarena@users.noreply.github.com>
2024-11-10 16:10:18 -0500

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -16,7 +16,10 @@ MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 
-# A little bit of scaffolding if you want to use it
+### A little bit of scaffolding if you want to use it
+
+
+# Gets JSON data from a URL
 def get_json(url: str) -> dict:
     """
     Given a properly formatted URL for a JSON web API request, return a Python JSON object containing the response to that request.
@@ -29,6 +32,7 @@ def get_json(url: str) -> dict:
     return data
 
 
+# Gets coordinates for a given place name 
 def get_lat_lng(place_name: str) -> tuple[str, str]:
     """
     Given a place name or address, return a (latitude, longitude) tuple with the coordinates of the given place.
@@ -36,20 +40,21 @@ def get_lat_lng(place_name: str) -> tuple[str, str]:
     See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
     encoded_place_name = urllib.parse.quote(place_name)
-    url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{encoded_place_name}.json?access_token={MAPBOX_TOKEN}&types=poi"
+    url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{encoded_place_name}.json?access_token={MAPBOX_TOKEN}&types=poi" # correctly restructures url
     data = get_json(url)
     coordinates = data["features"][0]["geometry"]["coordinates"]
     longitude, latitude = coordinates[0], coordinates[1]
     return str(latitude), str(longitude)
 
 
+# Gets the nearest MBTA station according to cooridnates
 def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     """
     Given latitude and longitude strings, return a (station_name, wheelchair_accessible) tuple for the nearest MBTA station to the given coordinates.
 
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
     """
-    url = f"https://api-v3.mbta.com/stops?filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance&api_key={MBTA_API_KEY}"
+    url = f"https://api-v3.mbta.com/stops?filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance&api_key={MBTA_API_KEY}" # correctly restructures url
     data = get_json(url)
     station_info = data["data"][0]["attributes"]
     station_name = station_info["name"]
@@ -57,6 +62,7 @@ def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     return station_name, wheelchair_accessible
 
 
+# FInds nearest MBTA stop for the given place name
 def find_stop_near(place_name: str) -> tuple[str, bool]:
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
